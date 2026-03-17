cc476b4a7009470d6e9f471c0a9ae835ece7981f
Extremum <vlados8118@gmail.com>
2026-03-17 22:59:29 +1000

    Second commit

--- quiz_site/quiz_site/settings.py
+++ quiz_site/quiz_site/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["155.212.138.102", "study.allianceservice.cc", "allianceservice.cc"]
+ALLOWED_HOSTS = ["155.212.138.102", "study.allianceservice.cc", "allianceservice.cc", "127.0.0.1"]
 
 
 # Application definition
