20fd46fee74e55d12e943b7c5740602be9619a42
Extremum <vlados8118@gmail.com>
2026-03-17 20:33:23 +1000

    Init commit

--- quiz_site/quiz_site/settings.py
+++ quiz_site/quiz_site/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["155.212.138.102"]
 
 
 # Application definition
@@ -124,10 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 
 MEDIA_URL = "/media/"
