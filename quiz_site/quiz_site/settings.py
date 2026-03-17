f3bdaba5e3f378611286f77db6aae68bffcd4ef5
Extremum <vlados8118@gmail.com>
2026-03-17 22:37:12 +1000

    Second commit

--- quiz_site/quiz_site/settings.py
+++ quiz_site/quiz_site/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", "155.212.138.102", "study.allianceservice.cc", "allianceservice.cc"]
+ALLOWED_HOSTS = ["155.212.138.102", "study.allianceservice.cc", "allianceservice.cc"]
 
 
 # Application definition
